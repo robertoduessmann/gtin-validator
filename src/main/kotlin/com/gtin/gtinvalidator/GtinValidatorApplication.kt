63b376d47cda65a5c906f7ac6b9d999ddfd350fd
Roberto Duessmann <roberto.duessmann@gmail.com>
2019-06-19 14:05:39 +0200

    Bootstrap Kotlin project

---
+++ src/main/kotlin/com/gtin/gtinvalidator/GtinValidatorApplication.kt
@@ -0,0 +1,11 @@
+package com.gtin.gtinvalidator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GtinValidatorApplication
+
+fun main(args: Array<String>) {
+	runApplication<GtinValidatorApplication>(*args)
+}
